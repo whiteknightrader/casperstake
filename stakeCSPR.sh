df61eb4e1f85a5266fbb9fb576ce8be22db68b36
whiteknightrader <46353849+whiteknightrader@users.noreply.github.com>
2021-05-16 23:20:48 +0530

    Update stakeCSPR.sh

--- stakeCSPR.sh
+++ stakeCSPR.sh
@@ -159,13 +159,13 @@ delegation_fee=3000000000
 #Standard Calculations do not delete
 delegation_fee_decimal=$(echo "scale=9; $delegation_fee/$mots" | bc)
 
-PUBLIC_KEY_HEX=$(sudo cat $HOME/casperKeys/public_key_hex)
-
 STATE_ROOT_HASH=$(casper-client get-state-root-hash --node-address http://198.23.235.165:7777 | jq -r '.result | .state_root_hash')
 
 PURSE_UREF=$(casper-client query-state --node-address http://198.23.235.165:7777 --key "$PUBLIC_KEY_HEX" --state-root-hash "$STATE_ROOT_HASH" | jq -r '.result | .stored_value | .Account | .main_purse')
 
 BALANCE_ERROR=$(casper-client get-balance --node-address http://198.23.235.165:7777 --purse-uref "$PURSE_UREF" --state-root-hash "$STATE_ROOT_HASH" | jq -r '.result | .balance_value' 2>&1 1>$HOME/balance)
+
+while [ ! -f $HOME/balance ]; do sleep 1; done
 
 BALANCE=$(sudo cat $HOME/balance)
 rm $HOME/balance
@@ -254,7 +254,7 @@ if [ $BALANCE -gt $delegation_fee ]; then
 	 
 	 while [ ! -f $HOME/transOut ]; do sleep 1; done
 	 
-	 cat transOut
+	 cat $HOME/transOut
 	 
 	 hash=$(jq .result.deploy_hash transOut)
 	 opt=$hash
