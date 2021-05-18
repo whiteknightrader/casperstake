65db6e119d46772b8bbee8f2d2e9244c59d2c954
whiteknightrader <46353849+whiteknightrader@users.noreply.github.com>
2021-05-19 02:35:48 +0530

    Update stakeCSPR.sh

--- stakeCSPR.sh
+++ stakeCSPR.sh
@@ -106,7 +106,9 @@ delegation_fee=3000000000
 #Standard Calculations do not delete
 delegation_fee_decimal=$(echo "scale=9; $delegation_fee/$mots" | bc)
 
-NODE_ERROR=$(casper-client get-state-root-hash --node-address http://198.23.235.165:7777 2>&1)
+NODE_ERROR=$(casper-client get-state-root-hash --node-address http://198.23.235.165:7777 2>&1 1>$HOME/testrpc)
+while [ ! -f $HOME/testrpc ]; do sleep 1; done
+rm $HOME/testrpc
 
 if [ -n "$NODE_ERROR" ]; then
       echo "${RED}Kindly check if Node is ACTIVE!${NC}"
